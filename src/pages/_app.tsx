1b04e0739dfb053e58f17da13af7c6498cd0e596
Guimarães Mahota <guimaraesmahota@gmail.com>
2020-10-28 22:30:39 +0200

    add order page

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -4,6 +4,7 @@ import { AppProps } from 'next/app'
 import { ThemeProvider } from 'styled-components'
 import GlobalStyle from '../styles/global'
 import theme from '../styles/theme'
+import 'moment-timezone'
 
 const MyApp: React.FC<AppProps> = ({ Component, pageProps }) => {
   return (
